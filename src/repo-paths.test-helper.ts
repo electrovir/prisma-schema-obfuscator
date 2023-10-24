d2c747c158ad09178a9789b22e07f8a318cb19a6
electrovir <electrovir@users.noreply.github.com>
2023-10-24 18:32:42 +0000

    Deploying to gh-pages from @ electrovir/prisma-schema-obfuscator@f9a89b9e8892dcb29fb26bd2ce04a049c8e0a2b3 🚀

--- src/repo-paths.test-helper.ts
+++
@@ -1,8 +0,0 @@
-import {join} from 'path';
-
-const testFilesDir = 'test-files';
-
-export const testFiles = {
-    singleModel: join(testFilesDir, 'example-schemas', 'single-model.prisma'),
-    singleEnum: join(testFilesDir, 'example-schemas', 'single-enum.prisma'),
-};
